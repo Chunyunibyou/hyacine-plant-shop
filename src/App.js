1f148f5ae82cdff52cb42e898c1fb4cb91094064
Yunie Ramirez <137275352+Chunyunibyou@users.noreply.github.com>
2025-10-22 12:01:25 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
-import ProductList from "./pages/ProductList"; // we’ll make this next 💕
-
-
-function ProductListPlaceholder() {
-  return <h2 style={{ textAlign: "center", marginTop: "50px" }}>Product page coming soon! 🌿</h2>;
-}
+import ProductList from "./pages/ProductList";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LandingPage />} />
-        <Route path="/products" element={<ProductListPlaceholder />} />
+        <Route path="/products" element={<ProductList />} />
       </Routes>
     </Router>
   );
