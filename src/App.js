5c66fe2d433d6c0b6b6400fd72fef6671305c51c
Yunie Ramirez <137275352+Chunyunibyou@users.noreply.github.com>
2025-10-22 11:58:16 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import ProductList from "./pages/ProductList"; // we’ll make this next 💕
+
+
+function ProductListPlaceholder() {
+  return <h2 style={{ textAlign: "center", marginTop: "50px" }}>Product page coming soon! 🌿</h2>;
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/products" element={<ProductListPlaceholder />} />
+      </Routes>
+    </Router>
   );
 }
 
