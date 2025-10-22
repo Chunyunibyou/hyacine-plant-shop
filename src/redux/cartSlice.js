7267867fd1cca91967262d523fcafc32298cc3e5
Yunie Ramirez <137275352+Chunyunibyou@users.noreply.github.com>
2025-10-22 12:01:50 +0800

    Create cartSlice.js

---
+++ src/redux/cartSlice.js
@@ -0,0 +1,19 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [],
+  },
+  reducers: {
+    addToCart: (state, action) => {
+      const existing = state.items.find((i) => i.id === action.payload.id);
+      if (!existing) {
+        state.items.push({ ...action.payload, quantity: 1 });
+      }
+    },
+  },
+});
+
+export const { addToCart } = cartSlice.actions;
+export default cartSlice.reducer;
